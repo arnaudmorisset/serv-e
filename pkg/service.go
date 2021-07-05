f02c74cf85c8e4eb2b2f3bca8ee90078b358eb04
k0pernicus <k0pernicus@0xc0ff33.me>
2021-07-05 20:49:47 +0200

    Added more errors

--- pkg/service.go
+++ pkg/service.go
@@ -4,3 +4,6 @@ import "errors"
 
 /// Returned if the template layout file has not been found
 var ErrCannotFindHTMLLayoutFile = errors.New("unable to find an HTML layout file")
+
+/// Returned if the body is unreadable
+var ErrCannotReadBody = errors.New("cannot read body input")
